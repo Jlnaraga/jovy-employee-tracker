73d7db085524e45ecf48b65512e0bf0d16b10b56
Jovy Ira <ira_bonching@yahoo.com>
2022-08-17 20:15:28 -0400

    added info to seeds.sql

--- Assets/schema.sql
+++ Assets/schema.sql
@@ -5,21 +5,27 @@ USE employee_db;
 
 
 CREATE TABLE department (
-  id INT NOT NULL,
-  first_name VARCHAR(30) NOT NULL
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30)
 );
 
 CREATE TABLE role (
-  id INT NOT NULL,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL ,
-  department_id INT NOT NULL
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT NOT NULL
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id),
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
 );
